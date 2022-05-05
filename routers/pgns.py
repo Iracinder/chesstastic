d39ca68d02f4629a817c2a98fb55ef2b0a03a6be
Pierre-Jean Vincent <pierre-jean.vincent@medigo.com>
2022-05-05 19:12:17 +0200

    report on success or failure of valid pgn file upload

--- routers/pgns.py
+++ routers/pgns.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, File, UploadFile
+import chess.pgn
+from fastapi import APIRouter, File, UploadFile, HTTPException
 
 from utils.dependencies import COURSE_LOCATION
 
@@ -13,8 +14,16 @@ async def list_pgn():
 @router.post("/")
 async def create_pgns(files: list[UploadFile] = File(...)):
     file_uploaded = []
+    failed_upload = []
     for file in files:
         content = await file.read()
-        (COURSE_LOCATION / file.filename).write_bytes(content)
-        file_uploaded.append(file.filename)
-    return file_uploaded
+        new_pgn = COURSE_LOCATION / file.filename
+        new_pgn.write_bytes(content)
+        try:
+            with new_pgn.open() as f:
+                chess.pgn.read_game(f)
+                file_uploaded.append(file.filename)
+        except Exception:
+            new_pgn.unlink()
+            failed_upload.append(file.filename)
+    return {'uploaded': file_uploaded, 'failed': failed_upload}
